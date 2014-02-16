45f0ef7a0e8e871bf644f34a690323d6672ae752
Dave Jachimiak <dave.jachimiak@gmail.com>
2014-02-16 14:13:30 -0500

    Extract usage help

--- src/todo.hs
+++ src/todo.hs
@@ -17,19 +17,39 @@ dispatch "add"    = add
 dispatch "bump"   = bump
 dispatch "remove" = remove
 dispatch "view"   = view
-dispatch command  = unknownCommand command
+dispatch unknown  = unknownCommand unknown
 
 help :: [String] -> IO ()
-help _ = synopsis
+help ("add":_)      = putStrLn addUsage
+help ("bump":_)     = putStrLn bumpUsage
+help ("remove":_)   = putStrLn removeUsage
+help ("view":_)     = putStrLn viewUsage
+help []             = synopsis
+help (unknown:args) = unknownCommand unknown args
+
+addUsage :: String
+addUsage = usageString "add filename \"task item\""
+
+bumpUsage :: String
+bumpUsage = usageString "bump filename task_number"
+
+removeUsage :: String
+removeUsage = usageString "remove filename task_number"
+
+viewUsage :: String
+viewUsage = usageString "view filename"
+
+usageString :: String -> String
+usageString string = "todo " ++ string
 
 synopsis :: IO ()
 synopsis = do
-    putStrLn "general usage: todo [-h | command arguments]"
+    putStrLn "general usage: todo [-h[command] | command arguments]"
     putStrLn ""
-    putStrLn "specific usage: todo add filename \"task item\""
-    putStrLn "                todo bump filename task_number"
-    putStrLn "                todo remove filename task_number"
-    putStrLn "                todo view filename"
+    putStrLn $ "specific usage: " ++ addUsage
+    putStrLn $ "                " ++ bumpUsage
+    putStrLn $ "                " ++ removeUsage
+    putStrLn $ "                " ++ viewUsage
 
 add :: [String] -> IO ()
 add [filename, item] = appendFile filename (item ++ "\n")
